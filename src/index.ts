82868ef538d5461fa566965accfa43755db28d38
Vasu7777py <Subbannavarvasu@gmail.com>
2022-05-03 12:55:01 +0530

    changed the project to express

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,20 @@
-console.log("HI");
+
+const express = require('express');
+
+const app = express();
+app.set('view engine', 'ejs');
+app.use("/css", express.static(__dirname + "/../public/css"));
+app.use("/img" ,express.static(__dirname + "/../public/images"));
+const PORT = 3000;
+
+app.get("/", (req, res) => {
+    res.render("index");
+});
+
+app.get("/calculator", (req, res) => {
+    res.render("calculator");
+});
+
+const server = app.listen(PORT, () => {
+    console.log("Server is running on port : " + PORT);
+});
